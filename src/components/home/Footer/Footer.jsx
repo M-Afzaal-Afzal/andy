e806516b45f18f574b429ed410bda216e9c632d0
M Afzaal Afzal <afzaal1721@gmail.com>
2021-11-04 00:25:24 +0500

    icons updated

--- src/components/home/Footer/Footer.jsx
+++ src/components/home/Footer/Footer.jsx
@@ -4,6 +4,8 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import {FaDiscord} from 'react-icons/fa';
 import {Box} from "@mui/material";
 
 function Footer() {
@@ -21,12 +23,19 @@ function Footer() {
             {/*</div>*/}
             <div className="footer-down">
                 <p><i class="far fa-copyright"></i> &copy; 2021 Influencericons</p>
-                <div className="social">
-                    <a href=""><FacebookIcon fontSize={'60px'}/></a>
-                    <a href=""><InstagramIcon fontSize={'60px'}/></a>
-                    <a href=""><TwitterIcon fontSize={'60px'}/></a>
-                    <a href=""><LinkedInIcon fontSize={'60px'}/></a>
-                </div>
+                <Box sx={{
+                    display: 'flex',
+                }} className="social">
+                    <a href="#"><FacebookIcon fontSize={'60px'}/></a>
+                    <a href="#"><InstagramIcon fontSize={'60px'}/></a>
+                    <a href="#"><TwitterIcon fontSize={'60px'}/></a>
+                    <a style={{
+                        width: '47px',
+                    }} href="#"><FaDiscord style={{
+
+                    }} size={60} fontSize={'60px'}/></a>
+                    <a href="#"><ContactPhoneIcon fontSize={'60px'}/></a>
+                </Box>
             </div>
         </div>
         </Box>
